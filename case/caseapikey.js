97758b756084d3a0f62b1b34f5e24db4c0ce610b
Danu57P <130752313+sadakodev2002@users.noreply.github.com>
2023-05-01 20:15:22 -0700

    Update caseapikey.js

--- case/caseapikey.js
+++ case/caseapikey.js
@@ -66,3 +66,83 @@ axios.get(`https://api.lolhuman.xyz/api/telestick?apikey=${apikey}&url=${args[0]
 conn.sendMessage(from, { sticker: { url: data.result.sticker.random() } })
 })
 break
+// ISLAMIC
+case 'listsurah':
+axios
+.get(`https://api.lolhuman.xyz/api/quran?apikey=${apikey}`)
+.then(({ data }) => {
+var text = 'List Surah:\n'
+for (var x in data.result) {
+text += `${x}. ${data.result[x]}\n`
+}
+reply(text)
+})
+.catch(console.error)
+break
+case 'alquran':
+if (args.length < 1) return reply(`Example: ${prefix + command} 18 or ${prefix + command} 18/10 or ${prefix + command} 18/1-10`)
+axios
+.get(`https://api.lolhuman.xyz/api/quran/${args[0]}?apikey=${apikey}`)
+.then(({ data }) => {
+var ayat = data.result.ayat
+var text = `QS. ${data.result.surah} : 1-${ayat.length}\n\n`
+for (var x of ayat) {
+text += `${x.arab}\n${x.ayat}. ${x.latin}\n${x.indonesia}\n\n`
+}
+text = text.replace(/<u>/g, '_').replace(/<\/u>/g, '_')
+text = text.replace(/<strong>/g, '*').replace(/<\/strong>/g, '*')
+reply(text)
+})
+.catch(console.error)
+break
+case 'alquranaudio':
+if (args.length == 0) return reply(`Example: ${prefix + command} 18 or ${prefix + command} 18/10`)
+conn.sendMessage(from, { audio: { url: `https://api.lolhuman.xyz/api/quran/audio/${args[0]}?apikey=${apikey}` }, mimetype: 'audio/mp4' })
+break
+case 'asmaulhusna':
+axios
+.get(`https://api.lolhuman.xyz/api/asmaulhusna?apikey=${apikey}`)
+.then(({ data }) => {
+var text = `No : ${data.result.index}\n`
+text += `Latin: ${data.result.latin}\n`
+text += `Arab : ${data.result.ar}\n`
+text += `Indonesia : ${data.result.id}\n`
+text += `English : ${data.result.en}`
+reply(text)
+})
+.catch(console.error)
+break
+case 'kisahnabi':
+if (args.length == 0) return reply(`Example: ${prefix + command} Muhammad`)
+axios
+.get(`https://api.lolhuman.xyz/api/kisahnabi/${full_args}?apikey=${apikey}`)
+.then(({ data }) => {
+var text = `Name : ${data.result.name}\n`
+text += `Lahir : ${data.result.thn_kelahiran}\n`
+text += `Umur : ${data.result.age}\n`
+text += `Tempat : ${data.result.place}\n`
+text += `Story : \n${data.result.story}`
+reply(text)
+})
+.catch(console.error)
+break
+case 'jadwalsholat':
+if (args.length == 0) return reply(`Example: ${prefix + command} Medan`)
+axios
+.get(`https://api.lolhuman.xyz/api/sholat/${args[0]}?apikey=${apikey}`)
+.then(({ data }) => {
+var text = `Wilayah : ${data.result.wilayah}\n`
+text += `Tanggal : ${data.result.tanggal}\n`
+text += `Sahur : ${data.result.sahur}\n`
+text += `Imsak : ${data.result.imsak}\n`
+text += `Subuh : ${data.result.subuh}\n`
+text += `Terbit : ${data.result.terbit}\n`
+text += `Dhuha : ${data.result.dhuha}\n`
+text += `Dzuhur : ${data.result.dzuhur}\n`
+text += `Ashar : ${data.result.ashar}\n`
+text += `Maghrib : ${data.result.imsak}\n`
+text += `Isya : ${data.result.isya}`
+reply(text)
+})
+.catch(console.error)
+break
