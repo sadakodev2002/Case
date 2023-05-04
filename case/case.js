1667670b94d7c3fb00201332a65fc177c00e6b44
Danu57P <130752313+sadakodev2002@users.noreply.github.com>
2023-05-04 07:36:32 -0700

    Update case.js

--- case/case.js
+++ case/case.js
@@ -1,145 +1,32 @@
-乂    *Tools Menu* 
+case 'hentaivid':{
+setReply(mess.waithentai)
+async function hentai() {
+return new Promise((resolve, reject) => {
+const page = Math.floor(Math.random() * 1153)
+axios.get('https://sfmcompile.club/page/'+page)
+.then((data) => {
+const $ = cheerio.load(data.data)
+const hasil = []
+$('#primary > div > div > ul > li > article').each(function (a, b) {
+hasil.push({
+title: $(b).find('header > h2').text(),
+link: $(b).find('header > h2 > a').attr('href'),
+ category: $(b).find('header > div.entry-before-title > span > span').text().replace('in ', ''),
+share_count: $(b).find('header > div.entry-after-title > p > span.entry-shares').text(),
+views_count: $(b).find('header > div.entry-after-title > p > span.entry-views').text(),
+type: $(b).find('source').attr('type') || 'image/jpeg',
+video_1: $(b).find('source').attr('src') || $(b).find('img').attr('data-src'),
+video_2: $(b).find('video > a').attr('href') || ''
+})
+})
+resolve(hasil)
+})
+})
+}
+let ah = await hentai()
+let nano = await pickRandom(ah)
+conn.sendMessage(from,{caption: '*HENTAI NYA KAK*',video:{url: nano.video_1}},{quoted:m})
 
-*≻*
 
-  
-  ╭─▸ 𝘚𝘦𝘢𝘳𝘤𝘩 𝘔𝘦𝘯𝘶
-  │
-  ├ lirik ✓ ${feat("lirik")? yes :no}
-  ├ grupwa✓ ${feat("grupwa")? yes :no}
-  ├ yts✓ ${feat("yts")? yes :no}
-  ├ whatmusic✓ ${feat("whatmusic")? yes :no}
-  ├ whatanime✓ ${feat("whatanime")? yes :no}
-  ├ google✓ ${feat("google")? yes :no}
-  ├ kbbi✓ ${feat("kbbi")? yes :no}
-  ├ weather✓ ${feat("weather")? yes :no}
-  ├ kodepos✓ ${feat("kodepos")? yes :no}
-  ├ brainly✓ ${feat("brainly")? yes :no}
-  ├ wallpaper✓ ${feat("wallpaper")? yes :no}
-  ├ quotes✓ ${feat("quotes")? yes :no}
-  ├ manga-search✓ ${feat("quotes")? yes :no}
-  ├ infogempa
-  │
-  ╰────────────˧
-  
-   ╭─▸ 𝘚𝘩𝘰𝘳𝘵 𝘜𝘳𝘭
-   │
-   ├ tinyurl✓ ${feat("tinyurl")? yes :no}
-   ├ bitly  ✓ ${feat("bitly")? yes :no}
-   ├ cutly  ✓ ${feat("bitly")? yes :no}
-   ├ tourl ✓  ${feat("tourl")? yes :no}
-   ├ anonfile
-   │
-   ╰────────────˧
-
-   ╭─▸ 𝘚𝘦𝘵𝘵𝘪𝘯𝘨 𝘉𝘰𝘵
-   │
-   ├ setprefix✓ ${feat("setprefix")? yes :no}
-   ├ setthumb✓ ${feat("setthumb")? yes :no}
-   ├ setppbot✓ ${feat("setppbot")? yes :no}
-   ├ setfakeimg✓ ${feat("setfakeimg")? yes :no}
-   ├ setimgreply✓ ${feat("setimgreply")? yes :no}
-   ├ setimginfo✓ ${feat("setimginfo")? yes :no}
-   ├ setreply✓ ${feat("setreply")? yes :no}
-   ├ setbio✓ ${feat("setbio")? yes :no}
-   ├ setgif✓ ${feat("setgif")? yes :no}
-   ├ setfakegif✓ ${feat("setfakegif")? yes :no}
-   ├ setimgweb    
-   ├ setimgdoc   
-   ├ setimgquoted 
-   ├ setmenu   
-   │
-   ╰────────────˧
-
-   ╭─▸ 𝘚𝘵𝘰𝘳𝘢𝘨𝘦
-   │
-   ├ adderror✓    
-   ├ addstik✓    
-   ├ addvn✓    
-   ├ delvn✓   
-   ├ delstik✓    
-   ├ clearallerror✓    
-   ├ liststik✓
-   ├ listerror✓
-   │
-   ╰────────────˧
-   
-   ╭─▸ 𝘐𝘴𝘭𝘢𝘮𝘪𝘤
-   │
-   ├ hadis✓    
-   ├ alquran✓
-   ├ tafsirsurah✓
-   ├ 
-   ├ 
-   ├ 
-   │
-   ╰────────────˧
-   
-   ╭─▸ 𝘎𝘢𝘮𝘦
-   │
-   ├ math
-   ├ 
-   ├ 
-   │
-   ╰────────────˧ 
-   
-   ╭─▸ 𝘈𝘯𝘰𝘯𝘺𝘮𝘰𝘶𝘴 𝘊𝘩𝘢𝘵
-   │
-   ├ anonymous
-   ├ start
-   ├ startchat
-   ├ keluar
-   ├ next
-   ├ sendkontak
-   ├ invite
-   ├ joinchat
-   │
-   ╰────────────˧ 
-
-   ╭─▸ 𝘌𝘯𝘤𝘳𝘺𝘱𝘵
-   │
-   ├ obfus
-   ├ deobfus
-   ├ packer
-   │
-   ╰────────────˧
-
-   ╭─▸ 𝘋𝘢𝘵𝘢𝘣𝘢𝘴𝘦
-   │
-   ├ addsewa
-   ├ ceksewa
-   ├ addowner
-   ├ addcmdowner
-   ├ addcmdlimit
-   ├ addcmdprem
-   ├ listowner
-   ├ listcmdowner
-   ├ listcmdlimit
-   ├ listcmdprem
-   ├ clearallerror
-   ├ clearalluser
-   ├ clearallban
-   ├ clearallblock
-   │
-   ╰────────────˧
-  
-   ╭─▸ 𝘖𝘸𝘯𝘦𝘳 𝘔𝘦𝘯𝘶
-   │
-   ├ join✓ ${feat("join")? yes :no}
-   ├ bc ${feat("bc")? yes :no}
-   ├ bcgc✓ ${feat("bcgc")? yes :no}
-   ├ setexif✓ ${feat("setexif")? yes :no}
-   ├ setlang✓ ${feat("setlang")? yes :no}
-   ├ react✓ ${feat("react")? yes :no}
-   ├ block✓ ${feat("block")? yes :no}
-   ├ unblock✓ ${feat("unblock")? yes :no}
-   ├ getcase✓ ${feat("getcaze")? yes :no}
-   ├ autolevel✓ ${feat("autolevel")? yes :no}
-   ├ listpc✓ ${feat("listpc")? yes :no}
-   ├ listgc✓ ${feat("listgc")? yes :no}
-   ├ delchat
-   ├ > evalcode
-   ├ => evalcode2
-   ├ $ executor
-   │
-   ╰────────────˧ 
+}
+break
